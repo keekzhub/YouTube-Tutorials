113fa3e3ad89367f799fdec3fea47b9c28078b94
keekzhub <96910111+keekzhub@users.noreply.github.com>
2021-12-31 00:50:51 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client({ intents: 32767 })
 
+const preifx = process.env['prefix']
+const token = process.env['token']
+
+client.on('ready, () => {
+          console.log('i am online')
+})
+client.on('messageCreate, async (message) => {
+if (message.content === '+ping') {
+  message.reply({ content: 'pong!' })
+}
+})
+client.login(token)
