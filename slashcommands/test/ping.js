dc4a8bf6b18dc364cff507a5239e541b5ef15559
keekzhub <96910111+keekzhub@users.noreply.github.com>
2022-01-03 19:50:06 -1000

    Delete slashcommands/test directory

--- slashcommands/test/ping.js
+++
@@ -1,9 +0,0 @@
-const { CommandInteraction, Client, MessageEmbed } = require('discord.js')
-
-module.exports = {
-  name: 'ping',
-  description: 'Displays the bots ping',
-  run: async (client, interaction, args) => {
-    await interaction.followUp({ content: "pong" })
-  }
-}
